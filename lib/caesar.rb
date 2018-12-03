3b91b0a624d06489f62ab0776b8f5c38b25a8193
Kasper Gacek <kasperg3971@hstat.org>
2018-12-03 04:53:58 +0000

    Caesar Cipher complete

---
+++ lib/caesar.rb
@@ -0,0 +1,41 @@
+def caesar_encode(string,offset)
+  # code
+  lower_arr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y", "z"]
+  lower = lower_arr.rotate(offset)
+  upper_arr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  upper = upper_arr.rotate(offset)
+  string.split("").collect do |letter|
+    if lower_arr.include?(letter)
+      letter = lower[lower_arr.index(letter)]
+       letter
+      elsif upper_arr.include?(letter)
+      letter = upper[upper_arr.index(letter)]
+       letter
+    else
+      letter
+  end
+  end.join("")
+end
+def caesar_decode(string,offset)
+    lower_arr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    lower = lower_arr.rotate(-offset)
+    upper_arr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    upper = upper_arr.rotate(-offset)
+    string.split("").collect do |letter|
+        if lower_arr.include?(letter)
+            letter = lower[lower_arr.index(letter)]
+             letter
+            elsif upper_arr.include?(letter)
+            letter = upper[upper_arr.index(letter)]
+           letter
+          else
+            letter
+  end
+  end.join("")
+end
+# # puts "What would you like to encode?"
+# # user_string = gets.chomp
+# # puts "What do you want the offset to be?"
+# # user_offset = gets.chomp.to_i
+# # puts caesar_encode(user_string,user_offset)
+# #puts caesar_decode(user_string,user_offset)
